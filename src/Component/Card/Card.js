11d79fd15d270df294d9ee60dfcede19cc987bff
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-23 07:23:33 -0500

    added styles and added my question and answer to it

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactCardFlip from 'react-card-flip';
+import './Card.css';
 
 class Card extends Component {
 
     static defaultProps = {
-        title: '',
-        description: '',
+        question: '',
+        answer: '',
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isFlipped: false,
+        }
+    }
+
+    handleFlip = () => {
+        this.setState({
+            isFlipped: !this.state.isFlipped,
+        })
     }
 
     render() { 
         return (
-            <div className='Card'>
-
-            </div>
+            <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection='horizontal' className='Card'>
+                <div className='card-front'>
+                    <p>{this.props.question}</p>
+                    <button onClick={this.handleFlip}>Learn about me</button>
+                </div>
+                <div className='card-back'>
+                    <p>{this.props.answer}</p>
+                    <button onClick={this.handleFlip}>Thank you.</button>
+                </div>
+            </ReactCardFlip>
         );
     }
 }
 
 Card.propTypes = {
-    title: PropTypes.string,
-    description: PropTypes.string,
+    // add props and their data types in here
+    question: PropTypes.string,
+    answer: PropTypes.string,
 }
 
 
