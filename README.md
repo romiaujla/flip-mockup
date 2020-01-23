7f4a3cc993efaf618fe1b2f3686244f30926e701
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-23 13:53:02 -0500

    changes style and edited readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
+
+# Flip Mockup
+
+A Flip Card explaining who I am and demonstrating my skills. 
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
-
-In the project directory, you can run:
 
 ### `npm start`
 
--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <Card
-        question={'Who Am I?'}
-        answer={'I am Raman Aujla, a passionate full stack developer who likes to code to solve real life problems. I recently graduated from Thinkful where I learned the modern javascript frameworks such as React, Node.js, Express and for managing data we got to learn PSQL. Other than this stack I am currently learning Angular and TypeScript. Having a strong knowledge of the computer science fundamentals has helped me a lot in my learning process. I am currently looking for a long term position in a company that is growing and where I can get to learn new things and grow as an individual with in the company and make some great friends along my new journey. I hope you like my FLIP card, looking forward to hearing from you.'}
+        question={'Who Is Raman Aujla?'}
+        answer={'A passionate full stack developer who likes to code to solve real life problems. A recent graduate from Thinkful where I learned the modern javascript frameworks such as React, Node.js, Express and for managing data we got to learn PSQL. Other than this stack I am currently learning Angular and TypeScript. Having a strong knowledge of the computer science fundamentals has helped me a lot in my learning process. I am currently looking for a long term position in a company that is growing and where I can get to learn new things and grow as an individual with in the company and make some great friends along my new journey. I hope you like my FLIP card, looking forward to hearing from you.'}
       >
       </Card>
     </div>
