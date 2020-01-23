619598d14d0aae65091b833af0e72e77d1e31b64
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-23 15:30:41 -0500

    edited paragraph text

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -8,8 +8,8 @@ function App() {
       <Card
         question={'Who Is Raman Aujla?'}
         answers={[
-          'A passionate full stack developer having strong computer science fundamentals. Recently graduated from Thinkful where I got to learn, React, Node.js, Express and PostgreSQL. On the side I have begun learning Angular and TypeScrip to add to my skill set. I also have a good understanding of Java, C++, Visual Basic, MySQL and ASP.Net.',
-          'I am currently looking for a long term patnership with an innovative and a growing company were I can bring my skills to use and grow alongside the company. I am really excited and thankful to have been interviewed by Mercury Studios. I hope you like my FLIP card and am looking forward to hearing from you.'
+          'A passionate full stack developer having strong computer science fundamentals. Recently graduated from Thinkful where I got to learn, React, Node.js, Express and PostgreSQL. On the side I have begun learning Angular and TypeScrip to add to my skill set. ',
+          'I am currently looking for a long term patnership with an innovative and a growing company were I can bring my skills to use and grow alongside the company. I am really excited and thankful to have been interviewed by Mercury Studios. I hope you like my FLIP card and am looking to move forward to the next steps.'
         ]}
       >
       </Card>
