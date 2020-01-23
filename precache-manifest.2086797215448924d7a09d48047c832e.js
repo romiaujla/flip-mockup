3afcb2ac32b75e70bacbe10164c3a574507e1242
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-23 14:10:04 -0500

    Updates

---
+++ precache-manifest.2086797215448924d7a09d48047c832e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747034d4da4bf51da347bc5a6159c383",
+    "url": "/flip-mockup/index.html"
+  },
+  {
+    "revision": "3ab2744424b562f4e22f",
+    "url": "/flip-mockup/static/css/main.f3303453.chunk.css"
+  },
+  {
+    "revision": "7cf700c82d5427b6b7a8",
+    "url": "/flip-mockup/static/js/2.ea227180.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/flip-mockup/static/js/2.ea227180.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ab2744424b562f4e22f",
+    "url": "/flip-mockup/static/js/main.9c397bf3.chunk.js"
+  },
+  {
+    "revision": "d41e58b24de5d0eab729",
+    "url": "/flip-mockup/static/js/runtime-main.2e947fbc.js"
+  }
+]);
