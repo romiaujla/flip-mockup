7b7ff6bcba946de25becc1b2a73dccd886d7de1f
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-23 14:14:23 -0500

    corrected grammar

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Card
         question={'Who Is Raman Aujla?'}
-        answer={'A passionate full stack developer who likes to code to solve real life problems. A recent graduate from Thinkful where I learned the modern javascript frameworks such as React, Node.js, Express and for managing data we got to learn PSQL. Other than this stack I am currently learning Angular and TypeScript. Having a strong knowledge of the computer science fundamentals has helped me a lot in my learning process. I am currently looking for a long term position in a company that is growing and where I can get to learn new things and grow as an individual with in the company and make some great friends along my new journey. I hope you like my FLIP card, looking forward to hearing from you.'}
+        answer={'A passionate full stack developer who likes to code, to solve real life problems. A recent graduate from Thinkful where I learned the modern javascript frameworks such as React, Node.js, Express and for managing data we got to learn PSQL. Other than this stack I am currently learning Angular and TypeScript. Having a strong knowledge of the computer science fundamentals has helped me a lot in my learning process. I am currently looking for a long term position in a company that is growing and where I can get to learn new things and grow as an individual with in the company and make some great friends along my new journey. I hope you like my FLIP card, looking forward to hearing from you.'}
       >
       </Card>
     </div>
